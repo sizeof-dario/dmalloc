4eb3e51ecbe0204ad5b8818cd51f13c71210e977
sizeof-dario <dimeo.dario.h@gmail.com>
2026-04-10 11:31:20 +0200

    Update doc

--- include/dmalloc.h
+++ include/dmalloc.h
@@ -1,6 +1,6 @@
 /*******************************************************************************
 *                                                                              *
-*   dmalloc hybrid arena allocator.                                            *
+*   dmalloc allocator.                                                         *
 *                                                                              *
 *       Project repository at https://github.com/sizeof-dario/dmalloc.git.     *
 *                                                                              *
@@ -15,36 +15,39 @@
 
 #include <stddef.h> // For size_t.
 
+typedef void darena_t;
+
 /*  Initializes the memory region pointed to by `backing_memory` and of size
     `capacity` to an arena. */
 int darenainit(void *backing_memory, size_t capacity);
 
 /*  Destroys the arena pointed to by `arena`. */
-int darenadestroy(void *arena);
+int darenadestroy(darena_t *arena);
 
 /*  Allocates `size` bytes of memory in the arena pointed to by `arena`. If
     `arena` is `NULL`, the memory is allocated on the heap. */
-void *dmalloc(size_t size, void *arena);
+void *dmalloc(size_t size, darena_t *arena);
 
 /*  Deallocates the memory pointed to by `p` from the arena pointed to by
     `arena`. If `arena` is `NULL`, the memory is deallocated from the heap. */
-void dfree(void *p, void *arena);
+void dfree(void *p, darena_t *arena);
 
 /*  Allocates memory for `n` objects of size `size` in the arena pointed to by
     `arena` and initializes it to `0`. If `arena` is `NULL`, the memroy is
     allocated on the heap. */
-void *dcalloc(size_t n, size_t size, void *arena);
+void *dcalloc(size_t n, size_t size, darena_t *arena);
 
 /*  Reallocates the memory pointed to by `p` from `src` to a new block of size
     `size` in `dest`. Both `src` and `dest` can either be pointers to arenas or
     have a value of `NULL` for the memory being reallocated, respectively, from
     or on the heap. */
-void *drealloc(void *p, size_t size, void *dest, void *src);
+void *drealloc(void *p, size_t size, darena_t *dest, darena_t *src);
 
 /*  Reallocates the memory pointed to by `p` from `src` to a block in `dest`
     that is big enough for `n` objects of size `size`. Both `src` and `dest` can
     either be pointers to arenas or have a value of `NULL` for the memory being 
     reallocated, respectively, from or on the heap. */
-void *dreallocarray(void *p, size_t n, size_t size, void *dest, void *src);
+void *dreallocarray(void *p, size_t n, size_t size, darena_t *dest, 
+                    darena_t *src);
 
 #endif // DMALLOC_H
